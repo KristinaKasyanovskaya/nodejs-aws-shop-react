2c28df0cda2b1037b0829d6b264cb0358e3e2998
KristinaKasyanovskaya <kristina_kasyanovskaya@epam.com>
2025-02-22 23:32:43 +0500

    fix: fix path url

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,6 +1,5 @@
 const API_PATHS = {
-  product:
-    "https://es5ah7j82j.execute-api.us-east-1.amazonaws.com/dev/products",
+  product: "https://es5ah7j82j.execute-api.us-east-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
